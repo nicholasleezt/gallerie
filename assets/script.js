9ce2630203cabfd17899a0981f1b5168ac3ef1d8
nicholaswzk <38720664+nicholaswzk@users.noreply.github.com>
2018-06-26 21:40:11 +0800

    commit 4

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,43 @@
-new ScrollReveal();
-window.sr = ScrollReveal({
-    reset: true
+function getScrollBarWidth(){
+    var body = $(body);
+    var currentWidth = body.width();
+
+    body.addClass("active");
+    var scrollBarWidth = body.width() - currentWidth;
+    body.removeClass("active");
+
+    if (scrollBarWidth > 0) {
+        body.css("margin-left", scrollBarWidth + "px");
+    }
+
+    return scrollBarWidth;
+}
+
+function toggleOverlay(scrollBarWidth) {
+    var $body = $("body");
+    var $overlay = $(".overlay");
+
+    if ($body.hasClass("active")){
+        $body.removeClass("active");
+        $overlay.removeClass("dropped");
+    }
+    else{
+        $body.addClass("active");
+        $overlay.addClass("dropped");
+        if (scrollBarWidth > 0) {
+            $body.css("margin-left", scrollBarWidth + "px");
+        }
+    }
+}
+
+$(function () {
+    setInterval(getScrollBarWidth, 100);
+
+    var scrollBarWidth = getScrollBarWidth();
+
+    $(".dropdown").on('click',function(){
+        toggleOverlay(scrollBarWidth);
+    });
 });
-sr.reveal('img', {
-    duration: 500,
-    viewFactor: 0.5
-});
+
+
