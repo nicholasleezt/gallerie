b585fe5fc93ba3e5516e542cbfaa1e52ae14e216
nicholaswzk <38720664+nicholaswzk@users.noreply.github.com>
2018-07-02 18:38:59 +0800

    add js to swap hero

--- assets/script.js
+++ assets/script.js
@@ -22,6 +22,8 @@ let toggleOverlay = (scrollBarWidth, overlay, body) => {
     }
 }
 
+let mobileHero = body => body.clientWidth < 768 ? document.querySelector(".hero").setAttribute('src','assets/photos/photo_7_md.jpg') : document.querySelector(".hero").setAttribute('src','assets/photos/photo_7_xl.jpg');
+
 document.addEventListener("DOMContentLoaded", function (event) {
     const body = document.querySelector("body");
     const overlay = document.querySelector(".overlay");
@@ -30,6 +32,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
     setInterval(getScrollBarWidth(body), 100);
 
     let scrollBarWidth = getScrollBarWidth(body);
+
+    mobileHero(body);
 
     button.addEventListener('click', (event) => {
         toggleOverlay(scrollBarWidth, overlay, body);
