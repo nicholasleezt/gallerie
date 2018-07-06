914b047138848f1e9625283cd501ba443fe2d4ff
nicholaswzk <38720664+nicholaswzk@users.noreply.github.com>
2018-07-06 16:12:24 +0800

    commented out button hover, fix content jump

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,22 @@
-let getScrollBarWidth = body => {
-    const currentWidth = body.clientWidth;
-    body.classList.add('active');
-    let scrollBarWidth = body.clientWidth - currentWidth;
-    body.classList.remove('active');
-    if (scrollBarWidth > 0) {
-        body.style.marginLeft = scrollBarWidth + "px";
-    }
-    return scrollBarWidth;
-}
-
-let toggleOverlay = (scrollBarWidth, overlay, body) => {
-    if (body.classList.contains("active")) {
-        body.classList.remove("active");
+let toggleOverlay = (overlay) => {
+    if (overlay.classList.contains("dropped")) {
         overlay.classList.remove("dropped");
     } else {
-        body.classList.add("active");
         overlay.classList.add("dropped");
-        if (scrollBarWidth > 0) {
-            body.style.marginLeft = scrollBarWidth + "px";
-        }
     }
 }
 
-let mobileHero = body => body.clientWidth < 768 ? document.querySelector(".hero").setAttribute('src','assets/photos/photo_7_md.jpg') : document.querySelector(".hero").setAttribute('src','assets/photos/photo_7_xl.jpg');
+//let mobileHero = body => body.clientWidth < 768 ? document.querySelector(".hero").setAttribute('src','assets/photos/photo_7_md.jpg') : document.querySelector(".hero").setAttribute('src','assets/photos/photo_7_xl.jpg');
 
 document.addEventListener("DOMContentLoaded", function (event) {
     const body = document.querySelector("body");
     const overlay = document.querySelector(".overlay");
     const button = document.querySelector(".dropdown");
 
-    setInterval(getScrollBarWidth(body), 100);
-
-    let scrollBarWidth = getScrollBarWidth(body);
-
-    mobileHero(body);
+    //mobileHero(body);
 
     button.addEventListener('click', (event) => {
-        toggleOverlay(scrollBarWidth, overlay, body);
+        toggleOverlay(overlay);
     });
 
     new ScrollReveal();
@@ -45,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     });
     sr.reveal('.img', {
         duration: 500,
-        viewFactor: 0.2
+        viewFactor: 0.2,
+        container:document.querySelector('.container')
     });
 });
