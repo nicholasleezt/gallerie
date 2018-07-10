fd11711a3ab9b2a56d3609c5d47b8e2a9eacdcaf
nicholaswzk <38720664+nicholaswzk@users.noreply.github.com>
2018-07-10 17:17:42 +0800

    commit 5

--- assets/flickr.js
+++ assets/flickr.js
@@ -1,34 +1,63 @@
-
-(function($) {
-
-    $(document).ready(function() {
-        getJSONData();
-    });
-
 function getJSONData() {
     var myAPIKey = "4b904993412019defefb0647b4ff7d64";
     var myID = "47655196@N03";
     var myNumPhotos = 15;
-    var myFormat = "json";
 
     var flickrURL = "https://api.flickr.com/services/rest/" +
-    "?method=flickr.people.getPhotos" +
-    "&api_key=" + myAPIKey +
-    "&user_id=" + myID +
-    "&per_page=" + myNumPhotos +
-    "&format=" + myFormat +
-    "&format=json&nojsoncallback=1";
+        "?method=flickr.people.getPhotos" +
+        "&api_key=" + myAPIKey +
+        "&user_id=" + myID +
+        "&per_page=" + myNumPhotos +
+        "&page=1"+
+        "&privacy_filter=1" +
+        "&format=json&nojsoncallback=1";
+    $.getJSON(flickrURL, successFn);
+}
+function successFn(result) {
+    console.log(result);
+    console.log("flickr request is a success");
+    $.each(result.photos.photo, function (i, photo) {
+        var originalSizeURL = "https://farm" +
+            photo.farm +
+            ".static.flickr.com/" +
+            photo.server + "/" +
+            photo.id + "_" +
+            photo.secret + ".jpg";
+        var title = photo.title;
+        if (window.innerWidth <= 768) {
+            if (i < 7) {
+                $("<img>").attr({
+                    "id":i+1,
+                    "src": originalSizeURL,
+                    "class": "img",
+                    "alt": title
+                }).appendTo("#col1");
+            } else {
+                $("<img>").attr({
+                    "id":i+1,
+                    "src": originalSizeURL,
+                    "class": "img",
+                    "alt": title
+                }).appendTo("#col2");
+            }
+        }
+        else{
+            if (i % 2 == 0) {
+                $("<img>").attr({
+                    "id":i+1,
+                    "src": originalSizeURL,
+                    "class": "img",
+                    "alt": title
+                }).appendTo("#col2");
+            } else {
+                $("<img>").attr({
+                    "id":i+1,
+                    "src": originalSizeURL,
+                    "class": "img",
+                    "alt": title
+                }).appendTo("#col1");
+            }
+        }
 
-   $.getJSON( flickrURL, successFn );
-
-  }
-
- function successFn(result) {
-   console.log(result);
-   console.log("flickr request is a success, do a backflip");
- }
-
- function errorFn(xhr, status, strErr) {
-   console.log(strErr);
- }
-})(jQuery);
+    });
+}
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-let toggleOverlay = (overlay) => {
+const toggleOverlay = (overlay) => {
     if (overlay.classList.contains("dropped")) {
         overlay.classList.remove("dropped");
     } else {
@@ -6,22 +6,91 @@ let toggleOverlay = (overlay) => {
     }
 }
 
+const sortPhotos = () => {
+    let imgNodeList = document.querySelectorAll(".img");
+    let imgArray = Array.prototype.slice.call(imgNodeList, 0);
+    let mobile = window.innerWidth <= 768 ? true : false;
+
+    imgArray.sort(function (a, b) {
+        return a.id - b.id;
+    });
+
+    for (let i = 0; i < imgArray.length; i++) {
+        if (mobile) {
+            if (i < 7) {
+                document.querySelector("#col1").appendChild(imgArray[i])
+            } else {
+                document.querySelector("#col2").appendChild(imgArray[i])
+            }
+        } else {
+            if (i % 2 == 0) {
+                document.querySelector("#col2").appendChild(imgArray[i])
+            } else {
+                document.querySelector("#col1").appendChild(imgArray[i])
+            }
+        }
+    }
+}
+
+function hideNav(lastScrollTop, overlay, container) {
+    var st = container.scrollTop;
+    if (st > lastScrollTop) {
+        if (overlay.classList.contains("hidden") == false) {
+            overlay.classList.add("hidden");
+        }
+    } else {
+        if (overlay.classList.contains("hidden")) {
+            overlay.classList.remove("hidden");
+        }
+    }
+    return st;
+}
+
+const throttled = (delay, fn) => {
+    let lastCall = 0;
+    return function () {
+        const now = (new Date).getTime();
+        if (now - lastCall < delay) {
+            return;
+        }
+        lastCall = now;
+        return fn();
+    }
+}
+
+function throttle(delay, fn) {
+    let lastCall = 0;
+    return function (args) {
+        const now = (new Date).getTime();
+        if (now - lastCall < delay) {
+            return;
+        }
+        lastCall = now;
+        return fn(args);
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
     const body = document.querySelector("body");
     const overlay = document.querySelector(".overlay");
     const button = document.querySelector(".dropdown");
+    const container = document.querySelector(".container");
+    let lastScrollTop = 0;
+
+    getJSONData();
+
+    const resizeEvent = (event) => sortPhotos();
+    const resizeHandler = throttled(400, resizeEvent);
+    window.addEventListener("resize", resizeHandler);
+
+    const scrollEvent = (event) => {
+        let st = hideNav(lastScrollTop, overlay, container);
+        lastScrollTop = st <= 0 ? 0 : st;
+    };
+    const scrollHandler = throttle(400, scrollEvent);
+    container.addEventListener("scroll", scrollHandler);
 
     button.addEventListener('click', (event) => {
         toggleOverlay(overlay);
-    });
-
-    new ScrollReveal();
-    window.sr = ScrollReveal({
-        reset: true
-    });
-    sr.reveal('.img', {
-        duration: 500,
-        viewFactor: 0.2,
-        container:document.querySelector('.container')
     });
 });
